b181d15e3cc897324cda1cdbccbb63bebc7cbeaa
Tibor5 <tibibibi.novakovic9@gmail.com>
2023-08-13 09:27:29 +0200

    Create component placeholders

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  location: string
+}>()
+</script>
+
+<template>
+  <v-app-bar title="{{ location }}" :elevation="2">
+    <template v-slot:append>
+      <v-btn icon="mdi-heart"></v-btn>
+      <v-btn icon="mdi-magnify"></v-btn>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
